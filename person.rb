ec8de328ebd5a7b3883d2809ab54d0c61b6dcaaa
Ben Sales <adebsa2401@gmail.com>
2023-01-27 13:47:52 +0100

    feat: Make name argument positional instead keyword argument

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ class Person
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, name: 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @name = name
     @age = age
     @parent_permission = parent_permission
