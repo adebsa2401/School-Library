00cd64fccd851915b1166f64946bf72d110ffbaf
Ben Sales <adebsa2401@gmail.com>
2023-01-30 10:47:57 +0100

    fix: set write access to instance variables in Person and Rental

--- person.rb
+++ person.rb
@@ -1,8 +1,8 @@
 require_relative 'nameable'
 
 class Person < Nameable
-  attr_reader :id, :rentals
-  attr_accessor :name, :age
+  attr_reader :id
+  attr_accessor :name, :age, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
